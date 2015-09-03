bc2dc48296862eafddc6bde2d653c2748487e844
Frank Barrera | farkbarn <frankb2320@gmail.com>
2015-09-03 10:56:00 -0430

    agregado

---
+++ plug_func.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Plugin de Funciones
+Plugin URI: http://bcpublicidad.com.ve/
+Description: Plugin para ubicar funciones generales que
+se colocan en <code>functions.php</code>
+Version: 0.1
+Autor: Frank Barrera
+Autor URI: http://bcpublicidad.com.ve
+License: GPLv3
+*/
+?>
